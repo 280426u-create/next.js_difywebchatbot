a2d8b21d5f4f256facd8eed4c0dc378b55bcf035
kanazawa <280426u@ugs.kochi-tech.ac.jp>
2026-05-05 18:52:24 +0900

    LPhennkou

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,5 @@
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* ロゴなど */}
-      </div>
-
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            <strong>AIチャットボットへようこそ</strong><br />
-            質問するとすぐに回答します！
-          </p>
-
-          <Link
-            href="/query"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>チャットを始める</span>
-            <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* 画像とか入れたければここ */}
-        </div>
-      </div>
-    </main>
-  );
+  redirect("/lp");
 }
